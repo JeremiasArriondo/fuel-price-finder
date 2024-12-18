22c2a60d8b7452fadf5f80fb393be433d370fdae
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2024-12-18 01:06:25 -0300

    feat: simple navbar and background

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Link from "next/link"
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +29,18 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <div className="bg-white pl-6 md:flex border-b border-gray-200 z-20 shadow-[0_0_15px_0_rgb(0,0,0,0.1)]">
+        <nav>
+          <Link
+            href="/"
+            className="mr-6 flex h-16 items-center space-x-2 xl:w-[236px] xl:border-r xl:pr-4"
+          >
+            <span className="text-stone-900 font-bold sm:inline-block">
+              Fuel Price Finder
+            </span>
+          </Link>
+        </nav>
+      </div>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image';
+import map from '../public/map.svg'
 import MapClientWrapper from './components/MapClientWrapper';
 
 export default async function Page() {
     return (
-        <div className="bg-white-700 mx-auto my-5 w-[98%] h-[480px]">
-            <MapClientWrapper />
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
+            <div className="fixed inset-0 overflow-hidden opacity-75 bg-[#f8fafb]">
+                <Image alt="World Map" src={map} fill={true} quality={100} />
+            </div>
+            <main className="flex flex-col items-center flex-1 px-4 sm:px-20 text-center z-10 pt-8 sm:pt-20">
+                <div className="bg-white-700 mx-auto my-5 w-[500px] h-[480px]">
+                    <MapClientWrapper />
+                </div>
+            </main>
         </div>
     );
 }
